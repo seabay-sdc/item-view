6797c10664a4af416d097e5ed3cdff0251218229
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-07-11 15:48:15 -0500

    update dns

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-13-59-156-36.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-223-115-104.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/seabay.pem',
       ref: 'origin/master',
       repo: 'https://github.com/baebay/item-view.git',
       path: '/home/ubuntu/seabay',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install && node server && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
